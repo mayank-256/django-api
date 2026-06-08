3880ece5cae24ad9d8cdcc65d3c0aa4f3b4f2f96
mayank-256 <mayank.yadav@thoughtwin.com>
2026-06-08 15:25:12 +0530

    views update

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from datetime import datetime
 
+def hello_api(request):
+    return JsonResponse({
+        "message": "Hello API"
+    })
 def time_api(request):
     return JsonResponse({
         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
